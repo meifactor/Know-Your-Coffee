3ef919ee52d1f8c6b5ad2659aecfd1950d09aab1
Mike Azure <mikeazure@Alessandras-MacBook-Pro-2.local>
2025-04-29 18:21:31 -0400

    improved interactive page: animation, svg, and styles

--- app/static/js/interactive_coffee.js
+++ app/static/js/interactive_coffee.js
@@ -1,68 +1,92 @@
 $(document).ready(function () {
-    let coffeeData = [];
-    let currentIndex = 0;
-    const $coffeeData = $("#coffee-data");
-    const $popup = $("#popup");
-  
-    function renderCoffee(index) {
-      const coffee = coffeeData[index];
-      const hotspots = coffee.hotspots || [];
-  
-      $coffeeData.html(`
-        <div class="text-center">
-          <h4>${coffee.name}</h4>
-          <div class="image-container">
-            <img src="${coffee.image}" alt="${coffee.name}" class="img-fluid mb-3">
-          </div>
-          <p>${coffee.description}</p>
-          <button id="continue-btn" class="btn btn-primary">Continue</button>
-        </div>
-      `);
-  
-      const $container = $coffeeData.find(".image-container");
+  let coffeeData = [];
+  let currentIndex = 0;
+  const $coffeeData = $("#coffee-data");
+  const $popup      = $("#popup");
+  const $continue   = $("#continue-btn");
+
+  // helper for popup
+  function showPopup(el, info) {
+    const offset = $(el).offset();
+    $popup
+      .text(info)
+      .css({
+        top:  offset.top  + "px",
+        left: offset.left + $(el).outerWidth() + "px"
+      })
+      .show();
+  }
+
+  function renderCoffee(index) {
+    currentIndex = index;
+    const coffee   = coffeeData[index];
+    const hotspots = coffee.hotspots || [];
+
+    // reset Continue button
+    $continue
+      .prop("disabled", true)
+      .removeClass("enabled")
+      .css({});
+
+    $coffeeData.html(`<div class="image-container"></div>`);
+    const $container = $coffeeData.find(".image-container");
+
+    $.get(coffee.image, function(svgData) {
+      const $svg = $(svgData).find("svg")
+        .addClass("img-fluid")
+        .attr({ width: 500, height: 500 });
+
+      $container.html($svg);
+
+      let clickedCount = 0;
+      const total = hotspots.length;
+
       hotspots.forEach(hs => {
-        const $hot = $('<div class="hotspot"></div>')
-          .css({ top: hs.y + 'px', left: hs.x + 'px' })
-          .attr('data-info', hs.info);
-        $container.append($hot);
+        const $region = $svg
+          .find(`[fill="${hs.fillColor}"]`)
+          .addClass("hotspot")
+          .data("clicked", false);
+
+        $region.on("click", function(e) {
+          e.stopPropagation();
+          showPopup(this, hs.info);
+
+          if (!$region.data("clicked")) {
+            $region.data("clicked", true);
+            clickedCount++;
+
+            if (clickedCount === total) {
+              // enable Continue
+              $continue
+                .prop("disabled", false)
+                .addClass("enabled");
+            }
+          }
+        });
+
+        $region.hover(
+          () => $region.addClass("pulse-animation"),
+          () => $region.removeClass("pulse-animation")
+        );
       });
-    }
-  
-    // Show popup next to hotspot
-    $(document).on('click', '.hotspot', function (e) {
-      e.stopPropagation();
-      const info = $(this).data('info');
-      const offset = $(this).offset();
-      $popup
-        .text(info)
-        .css({
-          top: offset.top + 'px',
-          left: offset.left + $(this).outerWidth() + 'px'
-        })
-        .show();
     });
-  
-    // Hide popup on outside click
-    $(document).on('click', () => {
-      $popup.hide();
-    });
-  
-    // Load data and render first item
-    $.getJSON("/learn/beverages/data", function (data) {
-      coffeeData = data;
-      renderCoffee(currentIndex);
-    }).fail(function () {
-      $coffeeData.html("<p class='text-danger'>Failed to load coffee data.</p>");
-    });
-  
-    // Continue button handler
-    $coffeeData.on("click", "#continue-btn", function () {
-      if (currentIndex < coffeeData.length - 1) {
-        currentIndex++;
-        renderCoffee(currentIndex);
-      } else {
-        $coffeeData.html("<h4 class='text-center'>You're done!</h4>");
-      }
-    });
+  }
+
+  // advance when enabled
+  $continue.on("click", () => {
+    if ($continue.prop("disabled") || !coffeeData.length) return;
+    $popup.hide();
+    renderCoffee((currentIndex + 1) % coffeeData.length);
   });
-  
+
+  // hide popup on outside click
+  $(document).on("click", () => $popup.hide());
+
+  // initial load
+  $.getJSON("/learn/beverages/data", function (data) {
+    coffeeData = data;
+    if (coffeeData.length) renderCoffee(0);
+  }).fail(function () {
+    $coffeeData.html("<p class='text-danger'>Failed to load coffee data.</p>");
+  });
+});
